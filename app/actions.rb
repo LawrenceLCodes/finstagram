3e8a4a033d960ef365a0b128bf91edd785c041d6
Lawrence Lee <lawman77@gmail.com>
2024-02-07 21:23:33 +0000

    W5D1 added login page, modified code to handle sessions for user when they login, added page redirects but logout is not working as intended for now.

--- app/actions.rb
+++ app/actions.rb
@@ -1,20 +1,71 @@
 
+# --- Helper methods --- 
+# used for accessing @current_user on every .erb file or any page of this web application
+helpers do
+  def current_user
+    User.find_by(session[:user_id])
+  end
+end
+
+# --- Index ---
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
 
-get '/signup' do 
-  erb(:signup)
+# --- Signup ---
+get '/signup' do # if a user navigates to the path "/signup" page,
+  @new_user = User.new # setup empty @user object
+  erb(:signup) # render "app/views/signup.erb"
 end
 
 post '/signup' do
-  # Checks if username and avatar are on the page in the forms, if one or both are missing it will fail
-  if params[:username].present? && params[:avatar].present?
-    new_user = User.new({username: params[:username], avatar_url: params[:avatar]})
-    new_user.save
-    "Validation SUCCESSFUL!"
+  # grab user input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+  
+  # instantiates a User and stores it in new_user variable
+  @new_user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  # if user validations pass and user is saved into new_user variable
+  if @new_user.save
+    # If new user record has been successfully created, redirect them to login
+    redirect to('/login')
   else
-    "Validation FAILED!"
+    # returns signup page to allow user to correct errors
+    erb(:signup)
   end
+end
+
+# --- Login ---
+get '/login' do # when a GET request comes into /login
+  erb(:login) #render app/views/login.erb
+end
+
+post '/login' do # when we submit a form with an action for /login
+  username = params[:username]
+  password = params[:password]
+
+  # 1. find the user by username
+  user = User.find_by(username: username)
+
+  # 2. If that user exists & the user's password matches the password input
+  if user && user.password == password
+    # Set the session hash to the user.id within their user record & redirect to index or homepage
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login has failed."
+    erb(:login)
+  end
+end
+
+# --- Logout ---
+get '/logout' do
+  # modifies session hash to remove :user_id data and sets it to nil or nothing
+  # redirects user to index or homepage
+  session[:user_id] = nil 
+  redirect to('/')
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
     has_many :comments
     # One user or post can have many likes
     has_many :likes
+
+    # used for signup form validation purposes
+    # email and username are checked for both uniqueness (each user must have a unique email and username, cannot be in user record for existing user) and presence (valid user records contain all four attributes)
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
 end
